04ffb74d92387f9424d0058fde3cf47fb638b715
Damien Garaud <damien.garaud@gmail.com>
2015-06-15 23:03:54 +0200

    introduce 'stations' and 'countries' namespaces

--- kumo/app.py
+++ kumo/app.py
@@ -11,9 +11,8 @@ app = Flask(__name__)
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, ui=False, title='Kumo',
           description='Air quality measures stations')
-ns = api.namespaces[0]
-ns.name = 'root'
-ns.description = ''
+ns_station = api.namespace('stations', description='Get stations from ID')
+ns_country = api.namespace('countries', description='Get stations from countries')
 
 @blueprint.route('/doc/', endpoint='doc')
 def swagger_ui():
@@ -22,7 +21,7 @@ def swagger_ui():
 app.register_blueprint(blueprint)
 app.register_blueprint(apidoc.apidoc)  # only needed for assets and templates
 
-@api.route('/stations/<int:station_id>')
+@ns_station.route('/<int:station_id>')
 @api.doc(responses={404: "Station ID not found"},
          params={'station_id': "Station ID"})
 class Station(Resource):
@@ -30,19 +29,19 @@ class Station(Resource):
     def get(self, station_id):
         return jsonize(station(station_id))
 
-@api.route('/stations')
+@ns_station.route('/')
 class Stations(Resource):
     @api.doc(description="Get all stations")
     def get(self):
         return jsonize(stations())
 
-@api.route('/countries')
+@ns_country.route('/')
 class Countries(Resource):
     @api.doc(description="Get countries")
     def get(self):
         return countries()
 
-@api.route('/countries/<string:name>')
+@ns_country.route('/<string:name>')
 @api.doc(responses={404: "Country not found"},
          params={'name': "Country"})
 class Country(Resource):
