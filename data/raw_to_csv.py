c5601e653b811fa84a42de4066cc5b5d02928170
Damien Garaud <damien.garaud@gmail.com>
2015-06-16 23:41:01 +0200

    extract raw CSV data and clean them up

--- data/raw_to_csv.py
+++ data/raw_to_csv.py
@@ -8,15 +8,41 @@ import pandas as pd
 
 
 FNAME = 'AirBase_v8_stations.csv'
-
+KEEP_COLS = ['station_european_code', 'country_name', 'station_name',
+             'type_of_station', 'station_longitude_deg', 'station_latitude_deg',
+             'station_altitude']
+RENAME_COLS = {
+    'country_name': 'country',
+    'station_european_code': 'code',
+    'station_name': 'name',
+    'station_longitude_deg': 'lon',
+    'station_latitude_deg': 'lat',
+    'station_altitude': 'height',
+    'type_of_station': 'type'
+}
 
 def read_data(fname):
     """Read data from the CSV file
 
     Return a pandas DataFrame
     """
-    return pd.read_csv(fname, sep='\t', index_col=0)
+    return pd.read_csv(fname, sep='\t')
 
+def filter_column(df):
+    """Remove then rename some columns
+    """
+    df = df.copy()
+    for colname in df:
+        if colname not in KEEP_COLS:
+            df.pop(colname)
+    df.rename_axis(RENAME_COLS, axis=1, inplace=True)
+    df['type'] = df['type'].str.lower()
+    return df
 
-def extract_data(fname, colname):
-    df = read_data(fname)
+def main(fname):
+    rawdf = read_data(fname)
+    df = filter_column(rawdf)
+    df.to_csv('airbase.csv')
+
+if __name__ == '__main__':
+    main(FNAME)
